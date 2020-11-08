112c562f19dddb65ae73a8ec4981ebc4f1c19c20
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-11-08 02:40:33 +0100

    Create WorkerTest.java

---
+++ Test/WorkerTest.java
@@ -0,0 +1,49 @@
+package reservaHotel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WorkerTest {
+
+	@Test
+	final void testGetDni() {
+		
+		Worker workers = new Employee("71962630T", "María", "Herrera", "Soho Boutique Opera", "Recepcionista");
+		
+		assertEquals("71962630T", workers.getDni());
+	}
+	
+	@Test
+	final void testGetName() {
+		
+		Worker workers = new Employee("71962630T", "María", "Herrera", "Soho Boutique Opera", "Recepcionista");
+		
+		assertEquals("María", workers.getName());
+	}
+	
+	@Test
+	final void testSurname() {
+		
+		Worker workers = new Employee("71962630T", "María", "Herrera", "Soho Boutique Opera", "Recepcionista");
+		
+		assertEquals("Herrera", workers.getSurname());
+	}
+	
+	@Test
+	final void testGeNameHotel() {
+		
+		Worker workers = new Employee("71962630T", "María", "Herrera", "Soho Boutique Opera", "Recepcionista");
+		
+		assertEquals("Soho Boutique Opera", workers.getNameHotel());
+	}
+	
+	@Test
+	final void testGetJob() {
+		
+		Worker workers = new Employee("71962630T", "María", "Herrera", "Soho Boutique Opera", "Recepcionista");
+		
+		assertEquals("Recepcionista", workers.getJob());
+	}
+
+}
