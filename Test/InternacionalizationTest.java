724d632e229c53f42cd53b6e160ffcdd190d9e75
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-11-17 00:37:14 +0100

    Create InternacionalizationTest.java

---
+++ Test/InternacionalizationTest.java
@@ -0,0 +1,19 @@
+package reservaHotel;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.jupiter.api.Test;
+
+class InternacionalizationTest {
+	
+	static ResourceBundle my_bundle = ResourceBundle.getBundle("MessagesBundle_fr_FR");
+
+	@Test
+	void test() {
+		
+		assertEquals("Enregistrer l'utilisateur", my_bundle.getString("id2"));
+	}
+
+}
