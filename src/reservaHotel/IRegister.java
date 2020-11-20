9baa0cc8e6395905f8fb448b475893bb8b6958b7
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-11-20 20:18:12 +0100

    Create IRegister.java

---
+++ src/reservaHotel/IRegister.java
@@ -0,0 +1,16 @@
+package org.reshotel;
+
+public interface IRegister {
+	
+	public String getDni();
+	public void setDni(String dni);
+	
+	public String getName();
+	public void setName(String name);
+
+	public String getPassword();
+	public void setPassword(String password);
+	
+	public String toString();
+
+}
