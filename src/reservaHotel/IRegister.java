eb5b60ba950dae8285577893289fca6be42fceed
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-12-04 17:59:00 +0100

    Update IRegister.java

--- src/reservaHotel/IRegister.java
+++ src/reservaHotel/IRegister.java
@@ -1,14 +1,14 @@
 package org.reshotel;
 
-public interface IRegister {
+public interface IRegister<T> {
 	
-	public String getDni();
+	public T getDni();
 	public void setDni(String dni);
 	
-	public String getName();
+	public T getName();
 	public void setName(String name);
 
-	public String getPassword();
+	public T getPassword();
 	public void setPassword(String password);
 	
 	public String toString();
